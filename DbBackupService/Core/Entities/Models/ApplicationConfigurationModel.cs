b469a5afebc52cacc78310bacbe9783aa104f038
Artur Lewandowski <LewanMordor@protonmail.com>
2023-08-16 12:47:46 +0200

    v 1.3
    Add new feature - Delete old backups. User can select for how many days backups will be stored.
    Setting name: "TimeInDaysToHoldBackups" in appsettings.json file.
    Officially fix windows version for making backups for postgresql. The problem was not creating backup on complex password. Now application is storing auth in .pgpass or pgpass.conf files (depends on platform). Also clears this file after job t not store these data anymore.

--- DbBackupService/Core/Entities/Models/ApplicationConfigurationModel.cs
+++ DbBackupService/Core/Entities/Models/ApplicationConfigurationModel.cs
@@ -5,4 +5,5 @@ public class ApplicationConfigurationModel
     public string? LogsFileName { get; set; } = string.Empty;
     public string? BackupSaveDirectory { get; set; } = string.Empty;
     public bool IncludeDateOfCreateLogFile { get; set; } = false;
+    public int TimeInDaysToHoldBackups { get; set; } = 2;
 }
