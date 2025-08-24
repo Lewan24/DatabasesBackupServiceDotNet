549e7709d2845531e66f0ce018decad3a5f934a0
Artur Lewandowski <LewanMordor@protonmail.com>
2025-08-24 19:32:25 +0200

    Implement custom theme. Implement additional auth layout for future login handling

---
+++ DbBackupService/Src/Bootstraper/Client/Client.UI/Data/Layout/DefaultLayout.cs
@@ -0,0 +1,30 @@
+﻿using MudBlazor;
+
+namespace Client.UI.Data.Layout;
+
+public static class DefaultLayout
+{
+    public static readonly MudTheme Theme = new MudTheme
+    {
+        PaletteDark = new PaletteDark
+        {
+            Primary = "#fa892e",
+            PrimaryLighten = "#ffa764",
+            PrimaryDarken = "#f57a00",
+            
+            TextPrimary = "#FFFFFF",
+            TextSecondary = "#E3E3E3",
+            TextDisabled = "#7D7D7D", 
+            
+            Dark = "#121212",
+            DarkDarken = "#0A0A0A",
+            DarkLighten = "#1C1C1C",
+            
+            Background = "#121212",
+            AppbarBackground = "#1C1C1C",
+            DrawerBackground = "#1C1C1C",
+            
+            Surface = "#1C1C1C"
+        }
+    };
+}
